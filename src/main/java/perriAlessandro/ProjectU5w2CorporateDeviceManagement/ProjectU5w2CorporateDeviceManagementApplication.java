1e0e4086d30a9cb82b2fb7578088f8ca0edc1eab
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-04-12 10:29:23 +0200

    Initial commit

---
+++ src/main/java/perriAlessandro/ProjectU5w2CorporateDeviceManagement/ProjectU5w2CorporateDeviceManagementApplication.java
@@ -0,0 +1,13 @@
+package perriAlessandro.ProjectU5w2CorporateDeviceManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectU5w2CorporateDeviceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectU5w2CorporateDeviceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/perriAlessandro/ProjectU5w2CorporateDeviceManagement/ProjectU5w2CorporateDeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package perriAlessandro.ProjectU5w2CorporateDeviceManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectU5w2CorporateDeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
